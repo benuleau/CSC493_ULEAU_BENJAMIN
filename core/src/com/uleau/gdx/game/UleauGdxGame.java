5c3d00d359494f29a997799c2435847142613889
Benjamin Uleau <benuleau@gmail.com>
2016-09-11 23:50:20 -0400

    Added packages game and util with respective files. Set background color of the game to cornflower blue

--- core/src/com/uleau/gdx/game/UleauGdxGame.java
+++ core/src/com/uleau/gdx/game/UleauGdxGame.java
@@ -1,33 +1,56 @@
 package com.uleau.gdx.game;
 
+import game.WorldController;
+import game.WorldRenderer;
+
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class UleauGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class UleauGdxGame implements ApplicationListener {
+	private static final String TAG=UleauGdxGame.class.getName();
 	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
+	private boolean paused;
+	
+	@Override public void create(){
+		//Set Libgdx log level to DEBUG
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		//Initialize controller and renderer
+		worldController=new WorldController();
+		worldRenderer=new WorldRenderer(worldController);
+		//Game world is active on start
+		paused=false;
 	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	@Override public void render(){
+		//Don't update game world when paused
+		if(!paused){
+			//Update the game world by the time that has passed since the last rendered frame
+			worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		//Sets the clear screen color to: Cornflower Blue
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		//Clears the screen
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+		//Render game world to screen
+		worldRenderer.render();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	@Override public void resize(int width, int height){
+		worldRenderer.resize(width, height);
 	}
+	@Override public void pause(){
+		paused=true;
+	}
+	@Override public void resume(){
+		paused=false;
+	}
+	@Override public void dispose(){
+		worldRenderer.dispose();
+	};
 }
---
+++ core/src/game/WorldController.java
@@ -0,0 +1,21 @@
+package game;
+
+/**
+ * @author Benjamin Uleau
+ *
+ */
+public class WorldController {
+	private static final String TAG=WorldController.class.getName();
+	
+	public WorldController(){
+		
+	}
+	
+	private void init(){
+		
+	}
+	
+	public void update(float deltaTime){
+		
+	}
+}
